e60bc49d1a404ab110c98f3e0fc4e21b4c4df215
Mateo Norena <npmatt23@gmail.com>
2017-09-22 21:00:52 -0500

    One minute man

--- one-minute-man/script.py
+++ one-minute-man/script.py
@@ -5,7 +5,7 @@ from datetime import datetime
 url = 'http://www.hacker.org/challenge/misc/minuteman.php'
 response = requests.get(url).text[13:]
 while "back later" in response:
-    print("not yet")
+    print(str(datetime.now()) + " not yet")
     time.sleep(60)
     response = requests.get(url).text[13:]
 print("I,ve got a solution!")
