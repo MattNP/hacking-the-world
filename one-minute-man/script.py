9c372f930ceba3351d8f7465177f994d3d86386e
Mateo Norena <npmatt23@gmail.com>
2017-09-22 20:57:51 -0500

    One minute man

--- one-minute-man/script.py
+++ one-minute-man/script.py
@@ -8,6 +8,7 @@ while "back later" in response:
     print("not yet")
     time.sleep(60)
     response = requests.get(url).text[13:]
+print("I,ve got a solution!")
 file = open('solution.txt','w')
 file.write((str(datetime.now()) + " " + response))
 file.close()
